51a3a3072ecf99bc54c7f5c9aab91812bb8509b1
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2023-10-25 15:56:06 +0100

    adds osm attribution

--- resources/js/components/FarmMap.vue
+++ resources/js/components/FarmMap.vue
@@ -17,6 +17,7 @@
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
+        attribution='&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       ></l-tile-layer>
 
       <div v-if="show">
