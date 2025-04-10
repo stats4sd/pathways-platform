ba220a7396536c449f6320ca5abe1a17e0225d32
Ciara <ciara@stats4sd.org>
2025-04-10 06:34:58 +0100

    fixes typos

--- app/Http/Requests/HumanCerealNeedRequest.php
+++ app/Http/Requests/HumanCerealNeedRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class OrgnaicFertiliserRequest extends FormRequest
+class HumanCerealNeedRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
