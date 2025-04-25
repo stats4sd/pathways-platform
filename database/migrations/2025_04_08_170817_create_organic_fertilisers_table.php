f334d3549c2be355d289cf10f30136adade0ce95
Ciara <ciara@stats4sd.org>
2025-04-25 22:07:07 +0100

    updates from planning forms

--- database/migrations/2025_04_08_170817_create_organic_fertilisers_table.php
+++ database/migrations/2025_04_08_170817_create_organic_fertilisers_table.php
@@ -15,13 +15,22 @@ return new class extends Migration
             $table->id();
             $table->foreignId('farm_id')->constrained('farms');
             $table->string('year', 4)->nullable();
+            
+            $table->decimal('quantite_fumure_organique')->nullable();
             $table->decimal('superficie_exploitation')->nullable();
             $table->decimal('protion_fertilisable')->nullable();
             $table->decimal('superficie_rotation')->nullable();
             $table->decimal('superficie_cycle')->nullable();
             $table->decimal('gap_annuel')->nullable();
             $table->decimal('gap_cycle')->nullable();
+            $table->decimal('gap_cycle_pour100')->nullable();
             $table->integer('nb_annee')->nullable();
+
+            $table->text('observation_vocal')->nullable();
+            $table->text('observation_video')->nullable();
+            $table->text('observation_text')->nullable();
+            $table->text('observation_image')->nullable();
+            $table->text('appreciation_observation')->nullable();
             $table->timestamps();
         });
     }
