cd6b658070fe63369fbdbbf8e6d4c5d60534791d
Ciara <ciara@stats4sd.org>
2025-05-27 15:49:27 +0100

    adds missing media library

--- app/Models/OrganicFertiliser.php
+++ app/Models/OrganicFertiliser.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class OrganicFertiliser extends Model
+class OrganicFertiliser extends Model implements HasMedia
 {
     use CrudTrait;
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $table = 'organic_fertilisers';
     protected $guarded = [];
