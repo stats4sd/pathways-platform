6339a7c7d530aa2be151c1201686de07fc7d3b09
Ciara <ciara@stats4sd.org>
2026-07-03 14:23:23 +0100

    fixes village error

--- app/Http/Controllers/FarmController.php
+++ app/Http/Controllers/FarmController.php
@@ -652,7 +652,7 @@ class FarmController extends Controller
 
         return [
             "chef_upa" => $details->chef_upa,
-            "village_id" => $details->village->nom,
+            "village_id" => $details->village?->nom,
             "superficie_cultive_upa" => $details->superficie_cultive_upa,
             "upa_membres" => $details->upa_membres,
             "nombre_charrues" => $details->nombre_charrues,
